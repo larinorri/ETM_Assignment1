e8d6045b3e315676c9e67af32660e33f095fce9c
larinorri <larinorri@fullsail.edu>
2014-04-04 17:35:42 -0400

    Menu
    Adds a clean & simple main menu to the project.

---
+++ UnityProject_LariNorri/Assets/RunGUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunGUI : MonoBehaviour {
+
+	// called once per frame
+	void OnGUI () {
+	
+		// size of window?
+		int centerX = Screen.width / 2;
+		int centerY = Screen.height / 2;
+		string title = "Lari Norri MGMS ETM Unity Project 04/04/2014";
+
+		// build components for main menu
+		GUI.color = new Color(0,0,0);
+
+		//GUI.matrix = Matrix4x4.Scale (new Vector3 (2, 2, 1));
+		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+		GUI.skin.label.fontSize = 20;
+		GUI.Label(new Rect (centerX - 300, centerY - 100, 600, 50), title);
+
+		// make a button to start the game
+		GUI.skin.button.fontSize = 30;
+		GUI.backgroundColor = new Color (0, 1, 1, 0.25f);
+		if (GUI.Button (new Rect (centerX - 150, centerY, 300, 50), "START GAME")) {
+			Application.LoadLevel(1);
+
+		}
+	}
+}
---
+++ UnityProject_LariNorri/Assets/SpinMenuCam.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinMenuCam : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Rotate( new Vector3( 0, Time.deltaTime, 0));
+
+	}
+}
