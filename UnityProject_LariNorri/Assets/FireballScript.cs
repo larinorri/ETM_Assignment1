a20a8eb775e7fa951459c87ff1c3d96730d7f626
larinorri <larinorri@fullsail.edu>
2014-04-06 15:13:33 -0400

    Interactivity
    Adds basic game play & launching projectiles, also improves visuals.

---
+++ UnityProject_LariNorri/Assets/FireballScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballScript : MonoBehaviour {
+	
+	//public GameObject theCamera;
+	private float life;
+
+	// Use this for initialization
+	void Start () {
+		life = 0;
+		//transform.rotation = theCamera.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * 10);
+		life += Time.deltaTime;
+		if (life > 3)
+			Destroy (gameObject);
+	}
+}
---
+++ UnityProject_LariNorri/Assets/LaunchFireBallScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchFireBallScript : MonoBehaviour {
+
+	public GameObject fireballToLaunch;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject fball = (GameObject)Instantiate (fireballToLaunch);
+			fball.transform.rotation = transform.rotation;
+		}
+
+	}
+}
