9648c6e7f4a0e0dd14084c129a32293d31e140b4
larinorri <larinorri@fullsail.edu>
2014-04-11 21:26:35 -0400

    Transfer Commit
    Contains some project settings & disabled network test code. Testing
    tilt is not possible on the iPad simulator. This commit is to get the
    latest version of the repo on my windows 8 machine to see if I can
    deploy to windows phone. I have an original iPad but it cannot be
    upgraded past iOS 5.1. Unfortunately Apple does not let you download
    older versions of Xcode unless you are a registered developer. Perhaps
    things will go smoother on the windows side.

---
+++ UnityProject_LariNorri/Assets/NetworkingScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkingScript : MonoBehaviour {
+
+	public string connectionIP = "127.0.0.1";
+	public int connectionPort = 25001;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		/*if (Network.peerType == NetworkPeerType.Disconnected)
+		{
+
+			GUI.Label(new Rect(10, 10, 200, 20), "Status: Disconnected");
+
+			GUI.skin.button.fontSize = 12;
+			if (GUI.Button(new Rect(10, 30, 120, 20), "Client Connect"))
+			{
+				Network.Connect(connectionIP, connectionPort);
+			}
+			if (GUI.Button(new Rect(10, 50, 120, 20), "Initialize Server"))
+			{
+				Network.InitializeServer(2, connectionPort, false);
+			}
+			else if (Network.peerType == NetworkPeerType.Client)
+			{
+				GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Client");
+				if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect"))
+				{
+					Network.Disconnect(200);
+				}
+			}
+		}*/
+	}
+}
